41d32ffafe7cdb972083c947d1fe7c8dbb8209fd
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-03-09 13:16:25 -0600

    added routes and concurrently running server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const env= require('dotenv').config();
 
 
 //const routes = require("./routes");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
@@ -19,12 +20,12 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-//app.use(routes);
+app.use(routes);
 
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);

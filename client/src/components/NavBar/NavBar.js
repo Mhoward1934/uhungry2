2c66345c15a4f784558dd489d0f44b643deef324
bdavis127 <brettdavistcb@gmail.com>
2019-03-09 14:12:15 -0600

    added Navbar change

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-end">
-                <a className="navbar-brand" href="/">uHungry?</a>
+                <a className="navbar-brand" href="/">uHungry</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
